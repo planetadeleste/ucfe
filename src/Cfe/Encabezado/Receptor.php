f11896c1b5a501c0ce62af1c2e2ee7b4847e3c24
Alvaro Canepa <bfpdevel@gmail.com>
2022-06-07 06:54:46 -0300

    - Add phpdoc comments

--- src/Cfe/Encabezado/Receptor.php
+++ src/Cfe/Encabezado/Receptor.php
@@ -5,19 +5,20 @@ namespace PlanetaDelEste\Ucfe\Cfe\Encabezado;
 use PlanetaDelEste\Ucfe\Traits\HasAttributeTrait;
 
 /**
- * @property string $CP
- * @property string $CiudadRecep
- * @property string $CodPaisRecep
+ * @property string $CP           Codigo Postal
+ * @property string $CiudadRecep  Ciudad
+ * @property string $CodPaisRecep Código de Pais según ISO 3166-1 alfa-2
  * @property string $CompraID
- * @property string $DeptoRecep
- * @property string $DirRecep
- * @property string $DocRecep
+ * @property string $DeptoRecep   Departamento
+ * @property string $DirRecep     Domicilio del Receptor
+ * @property string $DocRecep     Nro de Doc. del Receptor
  * @property string $DocRecepExt
  * @property string $InfoAdicional
  * @property string $LugarDestEnt
- * @property string $PaisRecep
- * @property string $RznSocRecep
- * @property string $TipoDocRecep
+ * @property string $PaisRecep    País
+ * @property string $RznSocRecep  Nombre o Razon Social del Recepto
+ * @property string $TipoDocRecep Tipo de Documento del Receptor (1: NIE, 2: RUC, 3: CI, 4:otro, 5 pasaporte, 6 DNI, 7
+ *           NIFE)
  */
 class Receptor
 {
